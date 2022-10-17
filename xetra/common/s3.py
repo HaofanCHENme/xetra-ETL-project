27496aec10093b71bc90e6257da17b40f6a366fc
HaofanCHENme <haofan.chen@essec.edu>
2022-10-17 15:49:39 +0200

    Wrapped up test code and set up for the program

--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -4,6 +4,12 @@ import logging
 
 import boto3
 
+import pandas as pd
+from io import StringIO, BytesIO
+
+from xetra.common.constants import S3FileTypes
+from xetra.common.custom_exceptions import WrongFormatException
+
 class S3BucketConnector():
     """
     Class for interacting with S3 Buckets
@@ -30,14 +36,61 @@ class S3BucketConnector():
 
         :param prefix: prefix on the s3 bucket that should be filtered with
         
-        return:
+        returns:
           files: list of all the file names containing the prefix in the key
         """
         files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
         return files
 
-    def read_csv_to_df(self):
-        pass
+    def read_csv_to_df(self, key: str, decoding: str = 'utf-8', sep: str = ','):
+        '''
+        reading a csv file from the S3 bucket and returning a dataframe
+        
+        :param key: key of the file that should be read
+        :encoding: encoding of the data inside the csv file
+        :sep: seperator of the csv file
+        
+        returns:
+             data_frame: Pandas DataFrame containing the data of the csv file
+        '''
+        self._logger.info('Reading file %s/%s/%s', self.endpoint_url, self._bucket.name, key)
+        csv_obj = self._bucket.Object(key=key).get().get('Body').read().decode(decoding)
+        data = StringIO(csv_obj)
+        data_frame = pd.read_csv(data, sep=sep)
+        return data_frame
 
-    def write_df_to_s3(self):
-        pass
+    def write_df_to_s3(self, data_frame: pd.DataFrame, key: str, file_format: str):
+        """
+        writing a Pandas DataFrame to S3
+        supported formats: .csv, .parquet
+
+        Args:
+            data_frame (pd.DataFrame): Pandas DataFrame that should be written
+            key (str): key of the saved file
+            file_format (str): format of the saved file
+        """      
+        if data_frame.empty:
+            self._logger.info('The dataframe is empty! No file will be written!')
+            return None
+        if file_format == S3FileTypes.CSV.value:
+            out_buffer = StringIO()
+            data_frame.to_csv(out_buffer, index=False)
+            return self.__put_object(out_buffer, key)
+        if file_format == S3FileTypes.PARQUET.value:
+            out_buffer = BytesIO()
+            data_frame.to_parquet(out_buffer, index=False)
+            return self.__put_object(out_buffer, key)
+        self._logger.info('The file format %s is not '
+        'supported to be written to s3!', file_format)
+        raise WrongFormatException
+    
+    def __put_object(self, out_buffer: StringIO or BytesIO, key: str):
+        '''
+        Helper function for self.write_df_to_s3()
+        
+        : out_buffer: StringIO | BytesIO that should be written
+        : key: target key of the saved file
+        '''
+        self._logger.info('Writing file to %s/%s/%s', self.endpoint_url, self._bucket.name, key)
+        self._bucket.put_object(Body=out_buffer.getvalue(), Key=key)
+        return True
